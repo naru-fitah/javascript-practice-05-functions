1e25caaa04d064a92cdd8b8b30bd0a6ec4a7ec59
narufitah <narufitah@gmail.com>
2025-03-30 21:01:47 +0200

    first

---
+++ script.js
@@ -0,0 +1,80 @@
+// 01. Write a function greet(name) that returns "Hello, [name]!".
+
+function greet(name) {
+    return "Hello, " + name + " !"
+}
+console.log(greet("jas"))
+
+// 02. Write a function add(a, b) that returns the sum of two numbers.
+
+function add(a, b) {
+    return sum = a + b
+}
+console.log(add(1,7))
+
+// 03. Write a function isEven(num) that returns true if the number is even.
+
+function isEven(num) {
+    if (num % 2 == 0) {
+        return true
+    } else {
+        return false
+    }
+}
+console.log(isEven(8))
+
+// 04. Write a function multiplyByTwo(num) that returns the number times 2.
+
+function multiplyByTwo(num) {
+    return num * 2
+}
+console.log(multiplyByTwo(10))
+
+// 05. Write a function sayHi() that returns "Hi there!" with no parameters.
+
+function sayHi() {
+    return "Hi there!"
+}
+console.log(sayHi())
+
+// 06. Write a function getFullName(firstName, lastName) that returns the full name as one string.
+
+function getFullName(firstName, lastName) {
+    return firstName + " " + lastName
+}
+console.log(getFullName("naru","fitah"))
+
+// 07. Write a function square(num) that returns the square of a number.
+
+function square(num) {
+    return num ** 2
+}
+console.log(square(3))
+
+// 08. Write a function isPositive(num) that returns true if the number is greater than 0.
+
+function isPositive(num) {
+    if (num > 0) {
+        return true
+    } else {
+        return false
+    }
+}
+console.log(isPositive(2))
+
+// 09. Write a function toFahrenheit(celsius) that converts Celsius to Fahrenheit.
+
+function toFahrenheit(celsius) {
+    return (celsius * 9/5) + 32
+}
+console.log(toFahrenheit(10))
+
+// 10. Write a function getGreeting(name, age) that returns "Hi [name], you are [age] years old!".
+
+function getGreeting(name, age) {
+return "Hi " + name + ", you are " + age + " years old!"
+
+}
+console.log(getGreeting("naru", 25))
+
+
